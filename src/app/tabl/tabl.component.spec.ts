6f81cd67749b5b98b46aa40142414df41c3e296f
unknown <dhia.mediouni@esprit.tn>
2022-04-09 00:39:52 +0200

    first commit

---
+++ src/app/tabl/tabl.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TablComponent } from './tabl.component';
+
+describe('TablComponent', () => {
+  let component: TablComponent;
+  let fixture: ComponentFixture<TablComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TablComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TablComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/tabl/tabl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabl',
+  templateUrl: './tabl.component.html',
+  styleUrls: ['./tabl.component.css']
+})
+export class TablComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
